a3a8aeb3f825d4946a231bc828affb1537fe5115
nawabsahab16 <nawab162004@gmail.com>
2023-11-20 16:32:25 +0530

    setting up the new next app

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+
+export default function Home() {
+  return (
+      <Button className="bg-emerald-400 text-white">
+          Testing!
+      </Button>
+    )
+} 
